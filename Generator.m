a5c264c74fcc9c9bec531cda4f383859e8fbee99
LachlanDow <lachlan.dow@hotmail.co.uk>
2021-02-04 15:32:04 +0000

    Generator Class
    Added input variable and constructo methods, generation of fram methods completed to be built upon in future revisions

--- Generator.m
+++ Generator.m
@@ -42,26 +42,63 @@ classdef Generator
             %UNTITLED7 Construct an instance of this class
             %   Detailed explanation goes here
             obj.mParms = mainParms;
-            obj.fState = FrameState;
+            obj.fState = CurrentFrameParameters;
             obj.absPosition = 0;
             obj.tiltFilter = LpFilter1(mainParms.sampleRate);
             obj.flutterTimeOffset = Resonator(mainParms.sampleRate);
-            obj.outputLpFilter = resonator(mainParms.sampleRate);
-            obj.outputLpFilter = obj.outputLpFilter.set(0,mainParms.sampleRate/2);
+            obj.outputLpFilter = Resonator(mainParms.sampleRate);
+            obj.outputLpFilter = obj.outputLpFilter.set(0,mainParms.sampleRate/2,1);
+            obj.initGlottalSource()
             
             obj.ASC = LpNoiseSource(mainParms.sampleRate);
-            ibj.ASP = LpNoiseSource(mainParms.sampleRate);
+            obj.ASP = LpNoiseSource(mainParms.sampleRate);
             obj.FSP = LpNoiseSource(mainParms.sampleRate);
             
             obj.NFC = Resonator(mainParms.sampleRate);
-            obj.NAFC  AntiResinator(mainParms.sampleRate);
+            obj.NAFC = AntiResinator();
+            obj.OFC = Resonator(mainParms.sampleRate);
+            
+            for i = 1:mainParms.maxOralFormants
+                obj.OFC(i) = Resonator(mainParms.sampleRate);
+            end
+            
+            obj.NFP = Resonator(mainParms.sampleRate);
+            obj.OFP = Resonator(mainParms.sampleRate);
+            for k = 1:mainParms.maxOralFormants
+                obj.OFP(k) = Resonator(mainParms.sampleRate);
+            end
+            
+            obj.DFP = DifferencingFilter();
+        end
+        
+        function obj = generateFrame(obj,fParmsIn,outBuf)
+            %METHOD1 Summary of this method goes here
+            %   Detailed explanation goes here
+            if(fParmsIn == obj.fParms)
+                error "Frame Parms must be different"
+            end
+            obj.newFParms = fParmsIn;
+            
+            while outPos < outBuf.length
+                if (obj.pState ~= PeriodState || obj.pState.positionInPeriod >= obj.pState.periodLength)
+                    obj.stertNewPeriod()
+                end
+          
+           outBuf(outPos) = obj.computeNextOutputSignalSample();
+           obj.pState.positionInPeriod = obj.pState.positionInPeriod + 1;
+           obj.absPosition = obj.absPosition + 1;
+           end
+           if (isnumeric(fParmsIn.gainDb))
+            adjustSignalGain(outBuf,fParmsIn.agcRmsLevel)
+           end   
+        end
+        
+        function obj = computeNectOutputSignalSample(obj)
             
         end
         
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
+        function obj = initGlottalSource()
+            
         end
     end
 end
