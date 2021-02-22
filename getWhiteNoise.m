a5630a3a602fad3f61e01716c4204ab3afac4701
LachlanDow <lachlan.dow@hotmail.co.uk>
2021-02-22 11:01:20 +0000

    Testing output sounds
    Vowels produced successfully, consonants still need work

--- getWhiteNoise.m
+++ getWhiteNoise.m
@@ -1,6 +1,7 @@
 function y = getWhiteNoise()
 %getWHiteNoise function returns value between -1 and 1
 %   uses rand function to to generate funcftion between -1 and 1
-y = -1+0.5*rand(1,1);
+y = -1+0.3*rand(1,1);
 end
 
+ 
