eab8e21d7db0fec413afedf07352bb3a3fdf45bb
LachlanDow <lachlan.dow@hotmail.co.uk>
2021-03-05 11:30:08 +0000

    Experimental Functions to create input frame parameters
    Functions createFrameParms() will produce arrays of FrameParms based on the inputted data
    create full word will take 3 sound parameters and create a word from them
    
    These functions are redundant not as they have been replaced by the create word function and sound class as these work better for the future of the project

--- createFrameParms.m
+++ createFrameParms.m
@@ -10,7 +10,20 @@ for i = 1:height(parmList)
     end
     for k = 4:6
         parmOutputList(i).oralFormantBW(k-3) = parmList(i,k);
-    end  
+    end
+    
+    if ~isempty(parmList(i,7))
+        for l = 7:11
+            parmOutputList(i).oralFormantDb(l-5) = parmList(i,l);
+        end
+        parmOutputList(i).parallelBypassDb = parmList(i,12);
+        parmOutputList(i).fricationDb = parmList(i,13);
+        parmOutputList(i).cascadeVoicingDb = parmList(i,14);
+        parmOutputList(i).parallelVoicingDb = parmList(i,14);
+        parmOutputList(i).tiltDb = parmList(i,15);
+    end
+    
+    
 end
 
 end
